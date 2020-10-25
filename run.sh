d7ef7a68deecfbc7be1572478734445a93703258
Korsaja <freeflowmc@mail.ru>
2020-10-25 11:28:54 +0400

    add run.sh

---
+++ run.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+# shellcheck disable=SC2004
+if (( $EUID != 0 )); then
+    echo "Rights are required to run the installation as root"
+    exit
+fi
+git clone https://github.com/Korsaja/wimark_test
+# shellcheck disable=SC2164
+cd wimark_test/
+docker-compose build && docker-compose up -d
+# shellcheck disable=SC2103
+cd ..
+rm -rf wimark_test/
+echo "Done."
